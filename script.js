32369dfe910d190bb59160159b51f9ad83724d33
Ujjaval Mehta <64920455+ujjaval77@users.noreply.github.com>
2020-11-25 12:20:43 +0530

    style update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-$(function(){    $('.display .side-nav-bar .navbar')
+$(function(){    $('.display .side-nav-bar .nav-text')
     .on('click', function(){
         var $el = $(this);
         var filterKey = $el.attr('data-filter-key');
         
-        $('.display .navbar').removeClass('selected');
+        $('.display .nav-text').removeClass('selected');
         $el.addClass('selected');
         $('.components-section').attr('data-filter', filterKey)
         AOS.refresh();
